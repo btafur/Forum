5ec7e160f1557cf589c4377d374e7df9fbd62e27
Bruno Tafur <bruno.tafur@gmail.com>
2013-02-28 09:13:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = 'ef0e6ff904c2dcb27acfe4bd763ed3417dcde9d01b1151c18b74a3ad7de26d79b03f53807d1f4d6e79348b1b9768c6a5c3891cc371f114cd1aeb1040e2542343'
